051e84df8f4347d4eff99a10b35a63f7040999e9
dkress <dominik.kress@mail.schwarz>
2021-06-03 16:50:22 +0200

    Create deploy directory and entrypoint for cf command

---
+++ deploy/entrypoint.sh
@@ -0,0 +1,8 @@
+cf api "$INPUT_CF_API"
+cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
+
+if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
+  cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
+fi
+
+sh -c "cf7 $*"
