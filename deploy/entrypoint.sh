cc2d002e1d3f6fb73172226f6ca0f1f30b1112fd
dkress <dominik.kress@mail.schwarz>
2021-06-03 18:53:44 +0200

    Add everything for running action to cf push

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 cf api "$CF_API"
-cf auth "$CF_USERNAME" "$CF_PASSWORD"
+cf auth "$CF_USER" "$CF_PASSWORD"
 
 if [ -n "$CF_ORG" ] && [ -n "$CF_SPACE" ]; then
   cf target -o "$CF_ORG" -s "$CF_SPACE"
 fi
 
 sh -c "cf7 $*"
+
